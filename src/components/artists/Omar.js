a286f06af4fbbc994f546e4250c6b665a8b0a41a
James Hancock <jhancock532@gmail.com>
2021-07-20 13:02:20 +0100

    omar description update

--- src/components/artists/Omar.js
+++ src/components/artists/Omar.js
@@ -11,6 +11,16 @@ class Omar extends React.Component {
         <h1 className="showcase__artwork-title">The Cozy Tapes</h1>
         <VideoEmbed url="https://www.youtube.com/watch?v=KB85n0S5GMM" subtitle="Final Piece"/>
 
+        <p className="showcase__paragraph">
+        For the last 10 months, Omar Hamouda has been developing a videography project which involves
+        the documentation of his daily life and activities during the pandemic, and where we have not
+        had access to university facilities. The project incorporates elements of videography, 
+        photography, music production, where they are all brought into the digital art realm. 
+        The documentation is split into segments – starting from September 2020 up until right 
+        before the final unit hand in date in July 2021. The projects main aim was to find 
+        creative outlets within the simple everyday lives we all lead.
+        </p>
+
         <h1 className="showcase__author-subtitle">By Omar Hamouda</h1>
 
         <p className="showcase__paragraph">
